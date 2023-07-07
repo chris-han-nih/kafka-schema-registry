b3da570318b258a0732c7cbbdc4b68a347fc66e9
dave.han <dave.han@nsuslab.com>
2023-07-07 16:08:52 +0900

    Refactoring

--- dotnet/mq/producer/Provider/KafkaProducer.cs
+++ dotnet/mq/producer/Provider/KafkaProducer.cs
@@ -8,20 +8,15 @@ using Confluent.SchemaRegistry.Serdes;
 public sealed class KafkaProducer
 {
    private readonly IProducer<string, GenericRecord> _producer;
-
-   private readonly ProducerConfig _producerConfig = new()
-                                             {
-                                                BootstrapServers = "localhost:29092", EnableIdempotence = true
-                                             };
+   private readonly ProducerConfig _producerConfig = new() { BootstrapServers = "localhost:29092", EnableIdempotence = true };
    private readonly SchemaRegistryConfig _schemaRegistryConfig = new() { Url = "http://localhost:8081" };
    private readonly AvroSerializerConfig _avroSerializerConfig = new() { BufferBytes = 1024, AutoRegisterSchemas = true };
 
    private KafkaProducer()
    {
       var schemaRegistry = new CachedSchemaRegistryClient(_schemaRegistryConfig);
-      var nihSchemaRegistry = new NihSchemaRegistryClient(schemaRegistry, 100);
+      var nihSchemaRegistry = new NihSchemaRegistryClient(schemaRegistry);
 
-      Console.WriteLine("--------- Creating producer ---------");
       _producer = new ProducerBuilder<string, GenericRecord>(_producerConfig)
                  .SetKeySerializer(Serializers.Utf8)
                  .SetValueSerializer(new AvroSerializer<GenericRecord>(nihSchemaRegistry, _avroSerializerConfig))
@@ -29,6 +24,6 @@ public sealed class KafkaProducer
    }
 
    private static readonly Lazy<KafkaProducer> _instance = new(() => new KafkaProducer());
-   public static KafkaProducer Instance => _instance.Value;
+   public static KafkaProducer Instance => KafkaProducer._instance.Value;
    public IProducer<string, GenericRecord> Producer => _producer;
 }
